252ef8c58681992277de96f88766319c7cf259f7
WelFerreira <48627317+WelFerreira@users.noreply.github.com>
2019-04-07 20:55:03 -0300

    Update contapalavras.php

--- contapalavras.php
+++ contapalavras.php
@@ -5,116 +5,37 @@
 <body>
 <?php
 
-#Listar
+#Analisador
 
-$directory = dir('c:/\xampp/\htdocs/\phpzip/\java');
+							$files = new FilesystemIterator("../", FilesystemIterator::UNIX_PATHS);
 
-# Extension Filter
-### $allowed_ext = array(".sample", ".png", ".jpg", ".jpeg", ".txt", ".doc", ".xls"); 
-$allowed_ext = array(".java");
+							$filtered = new RegexIterator($files, "/\.(java)$/i");
 
-$do_link = TRUE; 
-$sort_what = 0;
-$sort_how = 0;
+							foreach($filtered as $file){
 
+								//echo $file . "<br/>";
 
-# # #
-function dir_list($dir){ 
-    $i=0; 
-    $dl = array(); 
-    if ($hd = opendir($dir))    { 
-        while ($sz = readdir($hd)) {  
-            if (preg_match("/^\./",$sz)==0) $dl[] = $sz;$i.=1;  
-        } 
-    closedir($hd); 
-    } 
-    asort($dl); 
-    return $dl; 
-} 
-if ($sort_how == 0) { 
-    function compare0($x, $y) {  
-        if ( $x[0] == $y[0] ) return 0;  
-        else if ( $x[0] < $y[0] ) return -1;  
-        else return 1;  
-    }  
-    function compare1($x, $y) {  
-        if ( $x[1] == $y[1] ) return 0;  
-        else if ( $x[1] < $y[1] ) return -1;  
-        else return 1;  
-    }  
-    function compare2($x, $y) {  
-        if ( $x[2] == $y[2] ) return 0;  
-        else if ( $x[2] < $y[2] ) return -1;  
-        else return 1;  
-    }  
-}else{ 
-    function compare0($x, $y) {  
-        if ( $x[0] == $y[0] ) return 0;  
-        else if ( $x[0] < $y[0] ) return 1;  
-        else return -1;  
-    }  
-    function compare1($x, $y) {  
-        if ( $x[1] == $y[1] ) return 0;  
-        else if ( $x[1] < $y[1] ) return 1;  
-        else return -1;  
-    }  
-    function compare2($x, $y) {  
-        if ( $x[2] == $y[2] ) return 0;  
-        else if ( $x[2] < $y[2] ) return 1;  
-        else return -1;  
-    }  
+							}
 
-} 
+							//echo "END FIlesystemIterator .......<br/>";
 
-$i = 0; 
-while($file=$directory->read()) { 
-    $file = strtolower($file);
-    $ext = strrchr($file, '.');
-    if (isset($allowed_ext) && (!in_array($ext,$allowed_ext)))
-        {
-            // dump 
-        }
-    else { 
-        //$temp_info = stat($file);
-		$temp_info = 0;
-        $new_array[$i][0] = $file; 
-        $new_array[$i][1] = $temp_info[7]; 
-        $new_array[$i][2] = $temp_info[9]; 
-        $new_array[$i][3] = date("F d, Y", $new_array[$i][2]); 
-        $i = $i + 1; 
-        } 
-} 
-$directory->close(); 
+							//echo "START RecursiveDirectoryIterator ........<br/>";
 
-switch ($sort_what) { 
-    case 0: 
-            usort($new_array, "compare0"); 
-    break; 
-    case 1: 
-            usort($new_array, "compare1"); 
-    break; 
-    case 2: 
-            usort($new_array, "compare2"); 
-    break; 
-} 
+							$files = new RecursiveDirectoryIterator('../');
 
-$i2 = count($new_array); 
-$i = 0; 
+							$files = new RecursiveIteratorIterator($files);
 
-for ($i=0;$i<$i2;$i++) { 
-    if (!$do_link) { 
-        $line = $new_array[$i][0]; 
- 
-    }else{ 
-        $line = $new_array[$i][0]; 
+							$filtered = new RegexIterator($files, "/\.(java)$/i");
 
-    } 
-    //echo $line."<br>"; 
-//} 
+							foreach($filtered as $file){
+								$line = $file->getRealPath();
+								//echo $line."<br/>";
+
+						//}
 
 #Palavras
 
-$arquivo = "java/".$line;
+$arquivo = $line;
 $procura = " if ";
 $arrayArquivo = file($arquivo);
 $contador = 0;
